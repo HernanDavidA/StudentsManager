da5368d795afa8582d0961a53a7dc2de26012be5
HernanDavidA <142030442+HernanDavidA@users.noreply.github.com>
2025-02-14 20:40:23 -0500

    Mejora de estructura para el proyecto

--- js/server.js
+++ js/server.js
@@ -1,17 +1,14 @@
 const express = require("express");  // Express crea el servidor web y gestiona las peticiones
 const cors = require("cors");  // Permite la comunicación entre el servidor y el cliente
 const mysql = require("mysql2"); // Conexión a la base de datos MySQL
+const config = require("./config");  // Configuración del servidor
 
-const app = express();
+const app = express();  // Crea una instancia de Express
 app.use(cors());
 app.use(express.json());  // Permite la comunicación entre el servidor y el cliente en formato JSON
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "hernan",
-    database: "estudiantes"
-});
+const db = mysql.createConnection(config.db);
+
 
 db.connect(err => {
     if (err) {
@@ -21,52 +18,17 @@ db.connect(err => {
     console.log("Conectado a la base de datos MySQL");
 });
 
-app.get("/estudiantes", (res) => {
-    db.query("SELECT * FROM estudiante", (err, results) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json(results);
-    });
+
+const estudiantes = require('../modulos/estudiantes/rutas.js')(db);
+app.use('/api/estudiantes', estudiantes);
+
+
+
+app.listen(config.app.port, () => {
+    console.log(`Servidor corriendo en http://localhost:${config.app.port}`);
 });
 
 
-app.post("/estudiantes", (req, res) => {
-    const { nombre } = req.body;
-    db.query("INSERT INTO estudiante (nombre) VALUES (?)", [nombre], (err, result) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({ id: result.insertId, nombre });
-    });
-});
-
-app.delete("/estudiantes/:id", (req, res) => {
-    const { id } = req.params;
-    db.query("DELETE FROM estudiante WHERE id = ?", [id], (err) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({ message: "Estudiante eliminado" });
-    });
-});
 
 
-app.put("/estudiantes/:id", (req, res) => {
-    const { id } = req.params;
-    const { nombre } = req.body;
-    db.query("UPDATE estudiante SET nombre = ? WHERE id = ?", [nombre, id], (err) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({ message: "Estudiante actualizado" });
-    });
-});
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+module.exports = app;  // Exporta la instancia de Express para ser utilizada en otros módulos
